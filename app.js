0883eea6bba21c8fed7858697a180604409c5e63
Malintha1996 <malintha1996@gmail.com>
2018-08-24 15:43:28 +0530

    institute page created
    basic styles done

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ app.get('/',function(req,res){
 app.get('/contact',function(req,res){
   res.render('contact');
 });
+app.get('/institute',function(req,res){
+  res.render('institute_account');
+});
 app.get('/profile/:name',function(req,res){
     var data ={age:29,job:'ninja',hobbies:['eating','running','hiking']}
     res.render('profile',{person:req.params.name,data:data});
